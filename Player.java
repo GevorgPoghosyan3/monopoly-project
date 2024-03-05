2a8875f1a54af6da6c46b015a2d7db90d6a5e275
meri-grigoryan4 <162299253+meri-grigoryan4@users.noreply.github.com>
2024-03-05 15:44:23 +0400

    Update Player.java

--- Player.java
+++ Player.java
@@ -1,3 +1,8 @@
+/**
+ * Represents a player in the Monopoly game with attributes such as name, position, money, and owned properties.
+ * Methods include moving, buying properties, paying and receiving rent.
+ */
+
 public class Player{
 
 }
