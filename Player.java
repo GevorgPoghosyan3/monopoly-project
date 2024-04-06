f62941ad8e9cd7a48f4b32a8c02e69bd885252e5
mary <maryygrigoryan@gmail.com>
2024-04-06 12:03:03 +0400

    Trying git

--- Player.java
+++ Player.java
@@ -4,5 +4,52 @@
  */
 
 public class Player{
+    public enum Color{
+        RED, GREEN, BLUE, YELLOW, PURPLE, BLACK, WHITE, PINK;
+    }
+    private Color playerColor;
+    private int money;
 
+    public Player(){
+        this.money = 1500;
+        playerColor = Color.(int)Math.random() * 8 + 1;
+    }
+    public int getMoney(){
+        return this.money;
+    }
+    void move(int numSpaces);
+
+    void moveTo(int pos);
+
+    int position();
+
+
+    void excMoney(int money);
+
+    void toJail();
+
+    boolean stayJail();
+
+    void sellProp(Square sq);
+
+    void leaveJail();
+
+    boolean inJail();
+
+    void addJailFree(boolean chance);
+
+    boolean useJailFree();
+
+    int numJailFree();
+
+    int getAssets();
+
+    /* Input stuff */
+    boolean inputBool(Monopoly.State state);
+
+    int inputInt(Monopoly.State state);
+
+    int inputDecision(Monopoly.State state, String[] choices);
+
+    Player inputPlayer(Monopoly.State state, Player notAllowed);
 }
